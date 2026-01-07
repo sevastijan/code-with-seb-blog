490f8569684114415b914615d2b468bef5361132
Sebastian Ślęczka <me@sevastijan.com>
2026-01-07 20:46:50 +0100

    Fix security

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
-import nextTypescript from 'eslint-config-next/typescript'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+
+const nextCoreWebVitals = require('eslint-config-next/core-web-vitals')
+const nextTypescript = require('eslint-config-next/typescript')
+const eslintConfigPrettier = require('eslint-config-prettier')
 
 const eslintConfig = [
   ...nextCoreWebVitals,
